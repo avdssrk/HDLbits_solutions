e337a39da74b2835f5f62f5fc7719a4744dfc73d
Akula V D Siva Sai <66759301+avdssrk@users.noreply.github.com>
2023-07-28 21:54:40 +0530

    Create decade_counter.v

---
+++ Circuits/Sequential Circuits/Counters/decade_counter.v
@@ -0,0 +1,18 @@
+module top_module (
+    input clk,
+    input reset,        // Synchronous active-high reset
+    output [3:0] q);
+	
+    reg [3:0] count;
+    
+    always @(posedge clk) begin
+        if(reset) 
+            count<=0;
+        else if(count==9)
+            count<=0;
+        else
+            count<=count+1;
+    end
+    
+    assign q = count;
+endmodule
